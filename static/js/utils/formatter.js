776a25b91179697f1ca8039d7cdb987226e167b4
luizcodedthat <luiz2005eduardo@gmail.com>
2025-07-15 01:06:59 -0300

    renomeia metodo formatarCpfCnpj

--- static/js/utils/formatter.js
+++ static/js/utils/formatter.js
@@ -1,26 +1,26 @@
+export function formatarCpfCnpj (valor, tolerancia = 5) {
+  const valorLimpo = valor.replace(/\D/g, '').slice(0, 14);
 
-export function formatarCPFCNPJ(e, tolerancia = 5) {
-  let value = e.target.value.replace(/\D/g, ''); // Remove tudo que não for número
-  value = value.slice(0, 14); // Limita a 14 dígitos
-
-  if (value.length <= tolerancia) {
-    return
+  if (valorLimpo.length <= tolerancia) {
+    return valor;
   }
 
-  if (value.length <= 11) {
+
+
+  if (valorLimpo.length <= 11) {
     // Máscara CPF: 000.000.000-00
-    value = value
+    valor = valorLimpo
       .replace(/(\d{3})(\d)/, '$1.$2')
       .replace(/(\d{3})(\d)/, '$1.$2')
       .replace(/(\d{3})(\d{1,2})$/, '$1-$2');
   } else {
     // Máscara CNPJ: 00.000.000/0000-00
-    value = value
+    valor = valorLimpo
       .replace(/(\d{2})(\d)/, '$1.$2')
       .replace(/(\d{3})(\d)/, '$1.$2')
       .replace(/(\d{3})(\d)/, '$1/$2')
       .replace(/(\d{4})(\d{1,2})$/, '$1-$2');
   }
 
-  e.target.value = value;
+  return valor;
 }
